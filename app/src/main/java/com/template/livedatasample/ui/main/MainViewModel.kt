a5f1cb4ed0698be049a8cb0ca74fcb68fbcab38c
Reo Ando <ando.reo420@gmail.com>
2021-01-22 20:13:49 +0900

    #1 Transformations.map()使ってみる

--- app/src/main/java/com/template/livedatasample/ui/main/MainViewModel.kt
+++ app/src/main/java/com/template/livedatasample/ui/main/MainViewModel.kt
@@ -1,12 +1,16 @@
 package com.template.livedatasample.ui.main
 
+import android.icu.text.NumberFormat
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
 
 class MainViewModel : ViewModel() {
     private val _price = MutableLiveData<Int>()
-    val price: LiveData<Int> = _price
+    val price: LiveData<String> = Transformations.map(_price, {
+        NumberFormat.getCurrencyInstance().format(it)
+    })
 
     init {
         _price.value = 1000
